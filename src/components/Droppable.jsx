215e15c583d76e8b284a4ec95e79ac3ffeb32b22
Brendan Huddleston <brendanhuddleston18@gmail.com>
2024-04-06 09:23:04 -0500

    formatted

--- src/components/Droppable.jsx
+++ src/components/Droppable.jsx
@@ -15,22 +15,3 @@ export function Droppable(props) {
     </div>
   );
 }
-
-// import React from 'react';
-// import {useDroppable} from '@dnd-kit/core';
-
-// export function Droppable(props) {
-//   const {isOver, setNodeRef} = useDroppable({
-//     id: 'droppable',
-//   });
-//   const style = {
-//     color: isOver ? 'green' : undefined,
-//   };
-  
-  
-//   return (
-//     <div ref={setNodeRef} style={style}>
-//       {props.children}
-//     </div>
-//   );
-// }
